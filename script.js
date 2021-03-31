e4a8d8f09ad0640d60846d6903d8284879a23939
Natalya Myunster <natalyamyunster@yahoo.com>
2021-03-31 22:17:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+/* task 1 */
+
+// var str = '';
+// for (var i = 1; i <= 9; i++) {
+//     str = str + i;
+// }
+// console.log(str);
+
+/* task 2 */
+
+// var str = '';
+// for (var i = 9; i >= 1; i--) {
+//     str = str + i;
+// }
+// console.log(str);
+
+/* task 3 */
+
+// var str = '-';
+// for (var i = 9; i >= 1; i--) {
+//     str = '-' + i + str;
+// }
+// console.log(str);
+
+/*task 4 */
+
+// var str = '';
+// for (var i = 0; i < 10; i++) {
+//     str = str + 'x';
+//     console.log(str);
+// }
+
+/* task 5*/
+
+// for (var i = 1; i <= 20; i++) {
+//     var str = '';
+//
+//     for (var j = 1; j <= i; j++) {
+//         str = str + i;
+//     }
+//     console.log(str);
+// }
+
+
+/* task 6 */
+var str = '';
+for (var i = 0; i < 5 ; i++) {
+    str = str + 'xx';
+    console.log(str);
+}
+
